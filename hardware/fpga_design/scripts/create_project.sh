808aab780ba25491e83d034dabeede089ccb11ba
Florian Frank <Florian.Frank@uni-passau.de>
2025-10-16 15:10:32 +0200

    Add constraints file to create_project.tcl. Add shell script allowing to create the project

---
+++ hardware/fpga_design/scripts/create_project.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/opt/Xilinx/Vivado/2022.1/bin/vivado -mode batch -source tcl/create_project.tcl
--- hardware/fpga_design/scripts/tcl/create_project.tcl
+++ hardware/fpga_design/scripts/tcl/create_project.tcl
@@ -12,10 +12,13 @@ set project_dir "../memory_evaluator"
 set part_name "xczu9eg-ffvb1156-2-e"
 set bd_name "main_block_design"
 set bd_file "../block_designes/${bd_name}.tcl"
+set log_dir "../logs"
+
 
 set src_file_dir "${project_dir}/src"
 set sim_file_dir "${project_dir}/sim"
 set bd_file_dir "${project_dir}/bd"
+set constraints_file_dir "${project_dir}/constraints"
 
 # Create Project and subfolders
 if {[file exists $project_dir]} {
@@ -31,13 +34,16 @@ puts "INFO: Project $project_name created successfully."
 file mkdir $src_file_dir
 file mkdir $sim_file_dir
 file mkdir $bd_file_dir
+file mkdir $log_dir
+file mkdir $constraints_file_dir
 
-# Copy all source, sim files and block designes (TODO use files directly)
+# Copy all source, sim files  and block designes (TODO use files directly)
 file copy -force ../src $src_file_dir
 file copy -force ../sim $sim_file_dir
 file copy -force ../block_designes $bd_file_dir
+file copy -force ../constraints/constraints.xdc $constraints_file_dir/
 
-set verilog_files [get_verilog_files $src_file_dir]
+
 
 proc get_verilog_files {dir} {
     set files {}
@@ -54,6 +60,7 @@ proc get_verilog_files {dir} {
     return $files
 }
 
+set verilog_files [get_verilog_files $src_file_dir]
 foreach file $verilog_files {
     puts "INFO: Adding Verilog source: $file"
     add_files -norecurse $file
@@ -68,7 +75,17 @@ set_property ip_repo_paths [concat [get_property ip_repo_paths [current_project]
 update_ip_catalog
 
 
+# Add constraints file
+set constraints_file "${constraints_file_dir}/constraints.xdc"
+if {[file exists $constraints_file]} {
+    puts "INFO: Adding constraint file: $constraints_file"
+    add_files -fileset constrs_1 $constraints_file
+} else {
+    puts "WARNING: Constraint file not found: $constraints_file"
+}
+
 # Load block design
 if {[file exists $bd_file]} {
   source $bd_file
 }
+

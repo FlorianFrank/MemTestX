a533b2b4242fef23ae0023e52f37d24bb5625d35
Florian Frank <Florian.Frank@uni-passau.de>
2025-10-24 15:05:53 +0200

    Add command line parameter to run script

--- software/experiment_scheduler/scripts/run.sh
+++ software/experiment_scheduler/scripts/run.sh
@@ -1,8 +1,14 @@
 pushd ..
+
+  if [ $# -ne 1 ]; then
+    echo "No test config file provided!"
+    exit 1
+  fi
+
   if [ -d ".venv" ]; then
     source .venv/bin/activate
     pip3 install -r requirements.txt
-    python3 main.py -config_file=samples/sample_experiment_write_latency.yaml
+    python3 main.py -config_file="$1"
   else
     echo "No Virtual environment defined (run ./setup.sh)"
   fi
